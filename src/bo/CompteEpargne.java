30c24150b1435687f7f63e2175da6bf48eb34dce
Zenwinks <salle.lucas@hotmail.fr>
2019-10-08 17:01:09 +0200

    maj des classes metiers (WIP)

--- src/bo/CompteEpargne.java
+++ src/bo/CompteEpargne.java
@@ -1,4 +1,47 @@
 package bo;
 
 public class CompteEpargne extends Compte {
+    private float tauxInteret;
+
+    /**
+     * Construteur
+     */
+    public CompteEpargne() {
+    }
+
+    public CompteEpargne(int id, float solde, Agence agence, float tauxInteret) {
+        super(id, solde, agence);
+        this.tauxInteret = tauxInteret;
+    }
+
+    /**
+     * Getters and Setters
+     */
+    public float getTauxInteret() {
+        return tauxInteret;
+    }
+
+    public void setTauxInteret(float tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
+
+    /**
+     * toString()
+     */
+    @Override
+    public String toString() {
+        return "CompteEpargne{" +
+                "tauxInteret=" + tauxInteret +
+                ", id=" + id +
+                ", solde=" + solde +
+                ", agence=" + agence +
+                '}';
+    }
+
+    /**
+     * Methods
+     */
+    public float calculInteret() {
+
+    }
 }
--- src/bo/CompteSimple.java
+++ src/bo/CompteSimple.java
@@ -1,4 +1,40 @@
 package bo;
 
 public class CompteSimple extends Compte {
+    private float decouvertAutorise;
+
+    /**
+     * Construteur
+     */
+    public CompteSimple() {
+    }
+
+    public CompteSimple(int id, float solde, Agence agence, float decouvertAutorise) {
+        super(id, solde, agence);
+        this.decouvertAutorise = decouvertAutorise;
+    }
+
+    /**
+     * Getters and Setters
+     */
+    public float getDecouvertAutorise() {
+        return decouvertAutorise;
+    }
+
+    public void setDecouvertAutorise(float decouvertAutorise) {
+        this.decouvertAutorise = decouvertAutorise;
+    }
+
+    /**
+     * toString()
+     */
+    @Override
+    public String toString() {
+        return "CompteSimple{" +
+                "decouvertAutorise=" + decouvertAutorise +
+                ", id=" + id +
+                ", solde=" + solde +
+                ", agence=" + agence +
+                '}';
+    }
 }
