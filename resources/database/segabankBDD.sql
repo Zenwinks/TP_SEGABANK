ab9c0e42410a6bde612068c3b548f7c273f5aa45
sfondain <55797193+sfondain@users.noreply.github.com>
2019-10-12 16:38:09 +0200

    Add files via upload

---
+++ resources/database/segabankBDD.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  sam. 12 oct. 2019 à 14:30
+-- Version du serveur :  5.7.24
+-- Version de PHP :  7.2.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `segabank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `agence`
+--
+
+DROP TABLE IF EXISTS `agence`;
+CREATE TABLE IF NOT EXISTS `agence` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(30) NOT NULL DEFAULT '',
+  `adresse` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `agence`
+--
+
+INSERT INTO `agence` (`id`, `code`, `adresse`) VALUES
+(1, 'CA114', '2 rue de la modification'),
+(2, 'CA183', '18 rue des impasses'),
+(3, 'CA225', '2 avenue du champs'),
+(4, 'CA281', '141 boulevard du roseau');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+DROP TABLE IF EXISTS `compte`;
+CREATE TABLE IF NOT EXISTS `compte` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `solde` float NOT NULL,
+  `decouvertAutorise` float DEFAULT NULL,
+  `taux_interet` float DEFAULT NULL,
+  `type` int(1) NOT NULL,
+  `id_agence` int(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `compte`
+--
+
+INSERT INTO `compte` (`id`, `solde`, `decouvertAutorise`, `taux_interet`, `type`, `id_agence`) VALUES
+(1, 1000, 200, NULL, 1, 1),
+(2, 8000, NULL, 5, 2, 3),
+(3, 675, NULL, NULL, 3, 4),
+(4, 1400, 100, NULL, 1, 2),
+(5, 97213, NULL, 15, 2, 1);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
